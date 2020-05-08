991b82d04fdfd63218754150a7b4865a9f935f37
unknown <davidperezjpa@gmail.com>
2020-05-07 20:05:00 -0500

    Rotacion Objetos

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 "use strict"
+
+
 var canvas;
 var renderer;
 var scene;
@@ -10,9 +12,38 @@ var arr = [];
 var hasPlane = false;
 var cubos, esferas, cilindros, conos, tetrahedros, torus, torusknots, icosahedrons, dodecahedrons, octahedrons, geometriasCompuestas;
 var color;
+var isRotating = true;
+// var controls;
 
 function main()
-{
+{  
+    
+    
+    // var loader = OBJLoader();
+    // // load a resource
+    // loader.load(
+	// // resource URL
+	// '/3DModels/E 45 Aircraft_obj.obj',
+	// // called when resource is loaded
+	// function ( object ) {
+	// 	scene.add( object );
+	// },
+	// // called when loading is in progresses
+	// function ( xhr ) {
+	// 	console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+	// },
+	// // called when loading has errors
+	// function ( error ) {
+
+	// 	console.log( 'An error happened' );
+
+	// }
+    // );
+
+
+    
+    
+    
     cubos = 0;
     esferas = 0;
     cilindros = 0;
@@ -30,6 +61,7 @@ function main()
     canvas = document.getElementById("canvas");
     renderer = new THREE.WebGLRenderer({canvas: canvas});
     renderer.setSize(canvas.width, canvas.height);
+    document.body.appendChild(renderer.domElement);
     renderer.setClearColor("black");                    
 
     // LIGHTS
@@ -37,9 +69,15 @@ function main()
 
     // CAMERAS
     camera = new THREE.PerspectiveCamera(60., canvas.width / canvas.height, 0.01, 10000.);  // CAMERA
-    camera.position.set(-3, 3, 10);  
+    camera.position.set(-3, 3, 10);
     camera.up = new THREE.Vector3(0, 1, 0);
-    camera.lookAt(new THREE.Vector3(0, 0, 0));  
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    //Orbit Controlls
+    // controls = new THREE.OrbitControls(camera, renderer.domElement);
+    // controls.enableDamping = true;
+    // controls.campingFactor = 0.25;
+    // controls.enableZoom = true;
     
     // SCENE
     scene = new THREE.Scene();                                 
@@ -51,24 +89,58 @@ function main()
 
     // ACTION
     requestAnimationFrame(renderLoop);              // RENDER LOOP
+    
 }
 
 function onMouseMove( event ) {
-
     event.preventDefault();
-
     mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
     mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-
 }
+
+function playPauseRotation(){
+    if (isRotating){
+        isRotating = false;
+    }
+    else{
+        isRotating = true;
+    }
+    
+}
+
+function rotateObject() {
+    
+    for (var i =0 ; i<arr.length;i++){
+        if (arr[i].figura == "plane"){
+        }
+        else{
+            arr[i].obj.rotation.x = arr[i].obj.rotation.x + 0.01;
+            arr[i].obj.rotation.y = arr[i].obj.rotation.y + 0.01;
+        }  
+    }
+ }
+
+ function resetAnimation(){
+    for (var i =0 ; i<arr.length;i++){
+        if (arr[i].figura == "plane"){
+        }
+        else{
+            arr[i].obj.rotation.x = 0;
+            arr[i].obj.rotation.y = 0;
+        }  
+    }
+ }
        
 function renderLoop() {
     renderer.render(scene, camera);
+    // controls.update();
     if(sceneReady)
     {
          renderer.render(scene, camera);
-         mesh.rotation.x = mesh.rotation.x + 0.01;
-         mesh.rotation.y = mesh.rotation.y + 0.01;
+         if (isRotating){
+            rotateObject();
+         }
+         
     }
     requestAnimationFrame(renderLoop);
 }
